f82f998610f124b5997f3cb8e795793dc58ad53e
abhinavsalar04 <abhinav.salar99@gmail.com>
2025-02-01 23:00:49 +0530

    add user, video model, add custom methods in schema

---
+++ src/models/user.model.js
@@ -0,0 +1,95 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: [true, "Username must be unique"],
+      trim: true,
+      lowercase: true,
+      index: true, // this makes searching on username efficient, do not use this feature for each field
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: [true, "Email must be unique"],
+      lowercase: true,
+    },
+    fullName: {
+      type: String,
+      required: [true, "Full name is required"],
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // cloudinary url
+      required: [true, "Avatar is required"],
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+// while passing callback inside methods, use function keyword instead of arrow function
+// as arrow function does not remember the context of this we can not access schema fields
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+//  we can access these methods using model
+userSchema.methods.isPasswordCorrect = async function (password) {
+  // fisrt argument plane password, second argument hashed password
+  return await bcrypt.compare(password, this.password);
+};
+
+// usually jwt generation process is fast so no need to use async await
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiryIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiryIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
---
+++ src/models/video.model.js
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, // cloudinary url
+      required: [true, "Video file is required"],
+    },
+    thumbnail: {
+      type: String,
+      required: [true, "Thumbnail is required"],
+    },
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+    },
+    duration: {
+      type: Number, // cloudinary meta data
+      required: [true, "Duration is required"],
+    },
+    views: {
+      type: Number,
+      required: [true, "Views is required"],
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      required: [true, "isPublished is required"],
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Owner is required"],
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+export const Video = mongoose.model("Video", videoSchema);
