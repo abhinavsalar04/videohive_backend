9ae086bef202329fc226bccc2902e44be8a7a080
abhinavsalar04 <abhinav.salar99@gmail.com>
2025-02-02 13:04:58 +0530

    add couldinary utility & multer middleware

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export async function uploadOnCloudinary(localFilePath) {
+  try {
+    if (!localFilePath) throw new Error("File path not found!");
+
+    // uplaod file on cloudinary
+    const fileUploadResponse = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto", // detect file type from file
+    });
+
+    console.log(
+      "File uploaded successfully on cloudinary!",
+      fileUploadResponse
+    );
+    return fileUploadResponse;
+  } catch (error) {
+    // if file upload operation fails then remove temporary file from server
+    fs.unlinkSync(localFilePath);
+    throw new Error(error.message);
+  }
+}
